5e79f94467a91abfdb1b91bd48b1de6123d41302
evrey666 <shwarzman.art@yandex.ru>
2021-07-09 17:46:11 +0500

    выкладываю сюда

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+  $('.slider__content').slick({
+
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+
+    });
+  });
